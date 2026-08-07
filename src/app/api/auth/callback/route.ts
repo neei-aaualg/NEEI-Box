feacb7442cb24ad0512ee203c8a0069396dbd0e8
edagener0 <153123218+edagener0@users.noreply.github.com>
2026-08-07 15:58:29 +0100

    feat(courses): create page to visualize courses.

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -4,12 +4,11 @@ import { createClient } from '@/lib/supabase/server';
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
-  const next = searchParams.get('next') ?? '/';
+  const next = searchParams.get('next') ?? '/courses';
 
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-    console.log(error);
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`);
     }
